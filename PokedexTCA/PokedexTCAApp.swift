41cf971256bdd1e0caf925dd980f9b693be8e913
kuroyam <kuroyam1109@gmail.com>
2022-03-28 18:06:42 +0900

    Initial Commit

---
+++ PokedexTCA/PokedexTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexTCAApp.swift
+//  PokedexTCA
+//
+//  Created by Shun Kuroda on 2022/03/28.
+//
+
+import SwiftUI
+
+@main
+struct PokedexTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
