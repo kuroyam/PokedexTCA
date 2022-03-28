01937d637731d8e4d96aaf04fe193f674eea9d07
kuroyam <kuroyam1109@gmail.com>
2022-03-28 18:31:15 +0900

    雑にAPI叩く

--- PokedexTCA/ContentView.swift
+++ PokedexTCA/ContentView.swift
@@ -1,9 +1,32 @@
+import Combine
 import SwiftUI
 
 struct ContentView: View {
+
+    init() {
+        fetch()
+    }
+
     var body: some View {
         Text("Hello, world!")
             .padding()
+    }
+
+    func fetch() {
+        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon/1") else {
+            assertionFailure()
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                return
+            }
+
+            let pokemon = try! JSONDecoder().decode(Pokemon.self, from: data)
+            print(pokemon)
+        }
+        .resume()
     }
 }
 
@@ -11,4 +34,9 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Pokemon: Codable {
+    let id: Int
+    let name: String
 }
